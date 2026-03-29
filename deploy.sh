dd77a24b6f58d935af2bbf53937278fab86fd130
Hasan Noori <xishma@gmail.com>
2026-03-29 11:34:36 -0400

    Update Docker configurations and .dockerignore for production readiness
    - Expanded .dockerignore to include additional files and directories for better management of build artifacts, secrets, and environment files.
    - Modified Dockerfile and Dockerfile.ci to install production dependencies instead of local ones, ensuring the application is optimized for production environments.
    - Updated docker-compose.yml to specify Dockerfile.local for service builds, enhancing clarity in the build context for local development.

--- deploy.sh
+++ deploy.sh
@@ -5,7 +5,6 @@ ssh -o StrictHostKeyChecking=no -p $PORT $USER@$DOMAIN << 'ENDSSH'
   export $(cat .env | xargs)
   echo $CI_JOB_TOKEN | docker login -u $CI_REGISTRY_USER --password-stdin $CI_REGISTRY
   docker pull $APP_IMAGE
-  docker pull $PROXY_IMAGE
   docker compose -f docker-compose.prod.yml up --build -d
   if docker ps -a --format '{{.Names}}' | grep -q '^porfacan-web$'; then
     docker restart porfacan-web
